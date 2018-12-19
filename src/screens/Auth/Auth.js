ccce9536c0c4bf025dbd821afd4965ce5dd2d59b
osman.sevinc <osman.sevinc@etiya.com>
2018-12-19 15:46:44 +0300

    added validations

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -6,13 +6,36 @@ import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
 import MainText from '../../components/UI/MainText/MainText';
 import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground';
+import validate from '../../utility/validation';
 import backgroundImage from '../../assets/background.jpg';
 
 class AuthScreen extends Component {
 
     state = {
-        viewMode: Dimensions.get('window').height > 500 ? 'portrait' : 'landscape'
-
+        viewMode: Dimensions.get('window').height > 500 ? 'portrait' : 'landscape',
+        controls:{
+            email: {
+                value:'',
+                valid:false,
+                validationRules:{
+                    isEmail:true
+                }
+            },
+            password: {
+                value:'',
+                valid:false,
+                validationRules:{
+                    minLength:6
+                }
+            },
+            confirmPassword:{
+                value:'',
+                valid:false,
+                validationRules:{
+                    equalTo:'password'
+                }
+            }
+        }
     };
 
     constructor(props) {
@@ -34,6 +57,42 @@ class AuthScreen extends Component {
         startMainTabs();
     }
 
+    updateInputState = (key,value) =>{
+        let connectedValue = {};
+        if(this.state.controls[key].validationRules.equalTo){
+            const equalControl = this.state.controls[key].validationRules.equalTo;
+            const equalValue = this.state.controls[equalControl].value;
+            connectedValue = {
+                ...connectedValue,
+                equalTo: equalValue
+            }
+        }
+        if(key === 'password'){
+            connectedValue = {
+                ...connectedValue,
+                equalTo: value
+            }
+        }
+        this.setState(prevState =>{
+            return{
+                controls:{
+                    ...prevState.controls,
+                    confirmPassword:{
+                        ...prevState.controls.confirmPassword,
+                        valid: key === 'password' ? validate(prevState.controls.confirmPassword.value,prevState.controls.confirmPassword.validationRules,connectedValue):
+                            prevState.controls.confirmPassword.valid
+                    },
+                    [key]:{
+                        ...prevState.controls[key],
+                        value:value,
+                        valid:validate(value,prevState.controls[key].validationRules,connectedValue)
+                    }
+                }
+            }
+        });
+        console.log(this.state.controls)
+    };
+
     render() {
         let headingText = null;
         if (this.state.viewMode === 'portrait') {
@@ -51,16 +110,16 @@ class AuthScreen extends Component {
                     <ButtonWithBackground onPress={() => alert('Hello')} color="#d5ff80">Switch to
                         login</ButtonWithBackground>
                     <View style={styles.inputContainer}>
-                        <DefaultInput placeholder="E-Mail Address" style={styles.input}/>
+                        <DefaultInput placeholder="E-Mail Address" style={styles.input} value={this.state.controls.email.value} onChangeText={(val) => this.updateInputState('email',val)}/>
                         <View
                             style={this.state.viewMode === 'portrait' ? styles.portraitPasswordContainer : styles.landscapePasswordContainer}>
                             <View
                                 style={this.state.viewMode === 'portrait' ? styles.portraitPasswordWrapper : styles.landscapePasswordWrapper}>
-                                <DefaultInput placeholder="Password" style={styles.input}/>
+                                <DefaultInput placeholder="Password" style={styles.input} value={this.state.controls.password.value} onChangeText={(val) => this.updateInputState('password',val)}/>
                             </View>
                             <View
                                 style={this.state.viewMode === 'portrait' ? styles.portraitPasswordWrapper : styles.landscapePasswordWrapper}>
-                                <DefaultInput placeholder="Confirm Password" style={styles.input}/>
+                                <DefaultInput placeholder="Confirm Password" style={styles.input} value={this.state.controls.confirmPassword.value} onChangeText={(val) => this.updateInputState('confirmPassword',val)}/>
                             </View>
                         </View>
                     </View>
--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet,Animated } from "react-native";
 import {Navigation} from 'react-native-navigation';
 import { connect } from "react-redux";
 
@@ -7,6 +7,11 @@ import { connect } from "react-redux";
 import PlaceList from "../../components/PlaceList/PlaceList";
 
 class FindPlaceScreen extends Component{
+
+    state = {
+        placesLoaded:false,
+        removeAnim: new Animated.Value(1)
+    }
 
     itemSelectedHandler = key =>{
         const selPlace = this.props.places.find(place => {
@@ -30,14 +35,74 @@ class FindPlaceScreen extends Component{
         });
     };
 
+    placesSearchHandler = () =>{
+        Animated.timing(this.state.removeAnim,{
+            toValue:0,
+            duration:500,
+            useNativeDriver:true
+        }).start();
+    };
+
     render(){
+        let content = (
+            <Animated.View
+            style={{
+                opacity: this.state.removeAnim,
+                transform:[
+                    {
+                        scale:this.state.removeAnim.interpolate({
+                            inputRange:[0,1],
+                            outputRange:[12,1]
+                        })
+                    }
+                ]
+            }}>
+
+            <TouchableOpacity onPress={this.placesSearchHandler}>
+                <View style={styles.searchButton}>
+                    <Text style={styles.searchButtonText}>
+                        Find Places
+                    </Text>
+
+                </View>
+            </TouchableOpacity>
+
+            </Animated.View>
+        );
+
+        if(this.state.placesLoaded){
+            content =  <PlaceList places={this.props.places} onItemSelected={this.itemSelectedHandler}/>
+        }
+
         return(
-            <View>
-                <PlaceList places={this.props.places} onItemSelected={this.itemSelectedHandler}/>
+            <View style={this.state.placesLoaded ? styles.listContainer : styles.buttonContainer}>
+                {content}
             </View>
         );
     }
 };
+
+const styles = StyleSheet.create({
+    buttonContainer:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    listContainer:{
+
+    },
+    searchButton:{
+        borderColor:'purple',
+        borderWidth:3,
+        borderRadius:50,
+        padding:20
+    },
+    searchButtonText:{
+        color:'purple',
+        fontWeight:'bold',
+        fontSize:26
+    }
+})
 
 const mapStateToProps = state => {
     return {
